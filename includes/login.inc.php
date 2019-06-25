099419034cb821c6c4ff55c79c29f892efe34859
Qomplainerz <messanyrecordings@yahoo.de>
2019-06-25 23:47:39 +0200

    Update login.ing.php
    Replaced "passwd" with "Password"

--- includes/login.inc.php
+++ includes/login.inc.php
@@ -2,13 +2,13 @@
 if (isset($_POST['login'])) {
 require 'db.inc.php';
 $email = $_POST['Email'];
-$passwd = $_POST['Password'];
-if (empty($email) || empty($passwd)) {
+$Password = $_POST['Password'];
+if (empty($email) || empty($Password)) {
   header("location: ../index.php?error=emptyfields");
   exit();
 }
 else {
-  $sql = "SELECT *  FROM users WHERE username=? OR email=?;";
+  $sql = "SELECT * FROM users WHERE username=? OR email=?;";
   $stmt = mysqli_stmt_init($conn);
   if (!mysqli_stmt_prepare($stmt, $sql)) {
       header("location: ../index.php?error=sqlerror");
@@ -19,7 +19,7 @@ else {
     mysqli_stmt_execute($stmt);
     $result = mysqli_stmt_get_result($stmt);
     if ($row = mysqli_fetch_assoc($result)) {
-     $passwdcheck = password_verify($passwd, $row['pass']);
+     $passwdcheck = password_verify($Password, $row['pass']);
      if ($passwdcheck == false) {
        header("location: ../index.php?error=wrongpassword");
  exit();
